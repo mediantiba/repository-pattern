f3815357ca97f35e5eff8ac00a9cbd71899a7d0b
Mhd Median Tiba <mediantiba@hotmail.com>
2018-09-06 10:30:06 +0300

    Add required interfaces

---
+++ RepositoryPattern/Core/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RepositoryPattern.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // Add interfaces for other repositories here.
+
+        int Complete();
+    }
+}
